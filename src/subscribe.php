b842ef9c3b265105a3ba4fcd24161b9c538e3f2f
dotpegaso <dotpegaso@gmail.com>
2017-07-12 16:32:00 +0000

    mailchimp code

---
+++ src/subscribe.php
@@ -0,0 +1,40 @@
+<?php
+
+$data = [
+    'email'     => $_POST['email'],
+    'status'    => 'subscribed'
+];
+
+syncMailchimp($data);
+
+function syncMailchimp($data) {
+    $apiKey = '742f8e45abcdcd25b9d8299ae54adc3e-us16';
+    $listId = 'e8fdb4a991';
+
+    $memberId = md5(strtolower($data['email']));
+    $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+    $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+    $json = json_encode([
+        'email_address' => $data['email'],
+        'status'        => $data['status'] // "subscribed","unsubscribed","cleaned","pending"
+    ]);
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                                                                 
+
+    $result = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return $httpCode;
+}
+
+?>
