6a6020b384305155eb7064a212b0a7110404216f
dotpegaso <dotpegaso@gmail.com>
2017-07-12 16:37:16 +0000

    correcao de POST

--- src/newsletter.php
+++ src/newsletter.php
@@ -3,7 +3,7 @@
     <p>Quer saber tudo o que tá acontecendo e vai acontecer?</p>
     <p>Deixa teu email aí, vamos manter você atualizado!</p>
     <form action="/src/subscribe.php" method="POST" id="news">
-        <input type="email" name="newsletter" required/>
+        <input type="email" name="email" required/>
         <input type="submit" value="Eu quero!"/>
     </form>
 </section>
