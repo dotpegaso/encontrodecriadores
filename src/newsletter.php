632a714a7f52d0e145d82e1552d2faf468bebc2c
André L Benatti <dotpeg@gmail.com>
2017-07-12 16:32:00 +0000

    mailchimp code

--- src/newsletter.php
+++ src/newsletter.php
@@ -2,7 +2,7 @@
     <h1>Fique por dentro_</h1>
     <p>Quer saber tudo o que tá acontecendo e vai acontecer?</p>
     <p>Deixa teu email aí, vamos manter você atualizado!</p>
-    <form id="news">
+    <form action="/src/subscribe.php" method="POST" id="news">
         <input type="email" name="newsletter" required/>
         <input type="submit" value="Eu quero!"/>
     </form>
