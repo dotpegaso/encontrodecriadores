098b6fd3185fdaf8e6bcbbc76865a9bede42dcd0
dotpegaso <dotpegaso@gmail.com>
2017-07-13 18:10:42 +0000

    finalizacao email

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,23 +5,3 @@ $('a[href="#cadastro"]').on('click', function(){
 $('.navbar .fechar').on('click', function(){
    $('.navbar .modal').hide();
 });
-
-
-$('#edcData').on('submit', function(e){
-   e.preventDefault();
-   e.stopPropagation();
-   
-   $.ajax({
-      url: '../src/entry.php',
-      method: 'POST',
-      data: $('#edcData').serialize(),
-      beforeSend: function(){
-         $('.navbar .modal .overlay').show();
-      },
-      success:function(data){
-         $('.navbar .modal .overlay .begin').hide();
-         $('.navbar .modal .overlay .success').show();
-      }
-   });
-
-})
