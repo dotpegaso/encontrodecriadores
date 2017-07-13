a7ceb192f8fa11efca50c071d7158532321cb1f2
dotpegaso <dotpegaso@gmail.com>
2017-07-13 20:22:42 +0000

    sql fetch

--- src/dbconn.php
+++ src/dbconn.php
@@ -16,7 +16,7 @@
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                echo $row;
+                echo $row['nome'] . ' - ' . $row['responsavel'];
             }
         } else {
             echo "0 results";
