ec1295580cc0e61a151c8ff35518e50f5c21ab91
André L Benatti <dotpeg@gmail.com>
2017-07-13 21:01:39 +0000

    update

--- src/fetch_programacao.php
+++ src/fetch_programacao.php
@@ -1,6 +1,16 @@
 <?php
 
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+    $host = "mysql796.umbler.com";
+    $user = "heitor";
+    $pass = "fwxgy2hvxj";
+    $db = "dbedc";
+    $port = 3306;
+    
+    try{
+        $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+    }catch(Exception $e){
+        echo "ERRORCONN"; 
+    }
 
     $sql = "SELECT * FROM programacao WHERE ativo = true";
     $result = $connection->query($sql);
