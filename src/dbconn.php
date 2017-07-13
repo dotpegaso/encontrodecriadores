47b430616d2b1318b21421795589dc25710fd05f
dotpegaso <dotpegaso@gmail.com>
2017-07-13 20:26:35 +0000

    sql fetch

--- src/dbconn.php
+++ src/dbconn.php
@@ -10,11 +10,16 @@
         $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
         echo "Connected successfully"; 
         
-        $sql = "INSERT INTO programacao (nome, responsavel, horario, descricao, inscritos) VALUES ('Show Zeppelin', 'Page', 21, 'Descrição do show', 0)";
-        $connection->query($sql);
-        $connection->close();
+        $sql = "SELECT * FROM programacao";
+        $result = $connection->query($sql);
         
-        
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo $row['nome'] . ' - ' . $row['responsavel'];
+            }
+        } else {
+            echo "0 results";
+        }
 
     }catch(Exception $e){
         echo "ERRORCONN"; 
