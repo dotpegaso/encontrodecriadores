240cac691630c81b9ca0a6ff3143a792909fca3b
André L Benatti <dotpeg@gmail.com>
2017-07-13 20:08:57 +0000

    sql insert

--- src/dbconn.php
+++ src/dbconn.php
@@ -1,33 +1,30 @@
 <?php
-    
-    //Connect to the database
+
     $host = "mysql796.umbler.com";
-    $user = "heitor";                     //Your Cloud 9 username
-    $pass = "fwxgy2hvxj";                                  //Remember, there is NO password by default!
-    $db = "dbedc";                                  //Your database name you want to connect to
-    $port = 3306;                                //The port #. It is always 3306
+    $user = "heitor";
+    $pass = "fwxgy2hvxj";
+    $db = "dbedc";
+    $port = 3306;
     
     try{
         $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
         echo "Connected successfully"; 
         
-        $sql = "INSERT INTO programacao (descricao, horario, inscritos, nome, responsavel)
-        VALUES ('TesteDescricao', 14, 0, 'Show Matanza', 'John Cena')";
         
-        if ($connection->query($sql) === TRUE) {
-            echo "New record created successfully";
+        $sql = "SELECT * FROM programacao";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            
+            while($row = $result->fetch_assoc()) {
+                echo $row;
+            }
         } else {
-            echo "Error: " . $sql . "<br>" . $connection->error;
+            echo "0 results";
         }
         
-        $connection->close();
-        
+    }catch(Exception $e){
+        echo "ERRORCONN"; 
     }
-    catch(Exception $e)
-    {
-        var_dump($e);
-        echo "ERROR"; 
-    }
-    
 
 ?>
