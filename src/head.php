e3cd840004583307180a9ee8cb7bdd1921c67584
dotpegaso <dotpegaso@gmail.com>
2017-07-15 23:07:36 +0000

    finalizacao da home

--- src/head.php
+++ src/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#444444">
     <title>Encontro de criadores</title>
     <link rel="apple-touch-icon" sizes="57x57" href="/res/icon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/res/icon/apple-icon-60x60.png">
