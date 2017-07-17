a968ca61ecb561a19ae53f061f48f58676dc3656
dotpegaso <dotpegaso@gmail.com>
2017-07-17 22:33:11 +0000

    adicao de sites aos sponsors

--- src/sponsor.php
+++ src/sponsor.php
@@ -3,16 +3,16 @@
     <div class="container">
         <!--<div class="box"><img src="/res/santos.png" alt="" class="sponsor"></div>-->
         <!--<br>-->
-        <div class="box"><img src="/res/selo_criativo.png" alt="Selo Criativo" class="sponsor"></div>
-        <div class="box"><img src="/res/juicy.png" alt="Juicy Santos" class="sponsor"></div>
-        <div class="box"><img src="/res/arcano.png" alt="" class="sponsor"></div>
-        <div class="box"><img src="/res/bujas.png" alt="" class="sponsor"></div>
-        <div class="box"><img src="/res/dique.png" alt="" class="sponsor"></div>
+        <a href="https://www.selocriativo.com" target="_blank"><div class="box"><img src="/res/selo_criativo.png" alt="Selo Criativo" class="sponsor"></div></a>
+        <a href="https://www.juicysantos.com.br" target="_blank"><div class="box"><img src="/res/juicy.png" alt="Juicy Santos" class="sponsor"></div></a>
+        <a href="http://arcanozero.com" target="_blank"><div class="box"><img src="/res/arcano.png" alt="" class="sponsor"></div></a>
+        <a href="https://www.facebook.com/bujasbar/" target="_blank"><div class="box"><img src="/res/bujas.png" alt="" class="sponsor"></div></a>
+        <a href="https://www.artenodique.org/internacional" target="_blank"><div class="box"><img src="/res/dique.png" alt="" class="sponsor"></div></a>
         <!--<div class="box"><img src="/res/espaco.png" alt="" class="sponsor"></div>-->
         <!--<div class="box"><img src="/res/festival2.png" alt="" class="sponsor"></div>-->
-        <div class="box"><img src="/res/nove.png" alt="" class="sponsor"></div>
-        <div class="box"><img src="/res/janelas.png" alt="" class="sponsor"></div>
-        <div class="box"><img src="/res/revista.png" alt="" class="sponsor"></div>
+        <a href="http://www.revistanove.com.br" target="_blank"><div class="box"><img src="/res/nove.png" alt="" class="sponsor"></div></a>
+        <a href="http://www.janelas.tv.br" target="_blank"><div class="box"><img src="/res/janelas.png" alt="" class="sponsor"></div></a>
+        <a href="http://www.revistanove.com.br" target="_blank"><div class="box"><img src="/res/revista.png" alt="" class="sponsor"></div></a>
         
    </div>
 </section>
