c251892bfe362945c219ce19dfef05323b729532
dotpegaso <dotpegaso@gmail.com>
2017-07-18 13:02:57 +0000

    ogimage

--- src/head.php
+++ src/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#444444">
-    <meta property="og:image" content="https://image.ibb.co/czLjZa/facebook_ad.png" />
+    <meta property="og:image" content="https://image.ibb.co/meJTcv/facebook_ad.png" />
      <meta property="og:description" content="Somos uma plataforma multicultural de criatividade que através de eventos e ações, visa fomentar as expressões criativas como um todo, assim como valorizar a experiência de vida e a troca humana em todas suas vertentes!" />
     <title>Encontro de Criadores</title>
     <link rel="apple-touch-icon" sizes="57x57" href="/res/icon/apple-icon-57x57.png">
