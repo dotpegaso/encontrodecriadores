27afd20bf3cb7eb7055e7d5d66597bebb2c4cfcd
dotpegaso <dotpegaso@gmail.com>
2017-07-20 02:51:32 +0000

    atualizacao de programacao

--- programacao/index.php
+++ programacao/index.php
@@ -9,7 +9,7 @@
     
 
     <section id="intro" class="programacao">
-        <h1>Em breve <br><em>muita</em> coisa maluca e criativa</h1>
+        <h1>Programação <br><em>Encontro + Jazz</em></h1>
     </section>
 
     <div class="calendar">
@@ -19,129 +19,120 @@
 
     <section id="container">
         
-        <!--<div class="box">-->
-        <!--    <p class="date">21 Setembro</p>-->
+        <div class="box">
+            <p class="date">28 Julho - Sexta</p>
 
-        <!--    <div class="content">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--    </div>-->
+            <div class="content">
+                <p class="hour">19:00</p>
+                <br>
+                <p class="who">Rosa Marya & Banda Jazzileira</p>
+            </div>
 
-        <!--    <div class="content workshop">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--        <form>-->
-        <!--            <input type="email" name="" placeholder="Reserve sua vaga, digite seu email" required/>-->
-        <!--            <input type="hidden" name="nmworkshop" value="">-->
-        <!--            <button>QUERO!</button>-->
-        <!--        </form>-->
-        <!--    </div>-->
+           <div class="content">
+                <p class="hour">20:30</p>
+                <br>
+                <p class="who"> Saravá – 80 anos de Baden Powell – Quizumba Latina & Lucia Spivak (Argentina)</p>
+          </div>
 
-        <!--    <div class="content">-->
-                <!--<div class="break">-->
-                <!--    <p class="hour">19:00</p>-->
-                <!--    <p class="who">Coffee Break</p>-->
-                <!--</div>-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-         
-        <!--    </div>-->
+           <div class="content">
+                <p class="hour">22:00</p>
+                <br>
+                <p class="who">Ari Kumar Blues Band (USA)</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">23:00</p>
+                <br>
+                <p class="who pink">Liniker e os Caramelows</p>
+           </div>
+        </div>
+        
+        <div class="box">
+            <p class="date">29 Julho - Sábado</p>
 
-        <!--    <div class="content">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--    </div>-->
+            <div class="content">
+                <p class="hour">13:00</p>
+                <br>
+                <p class="who"> DJs Santos Jazz + Encontro de Criadores + Foods (full time)</p>
+            </div>
 
-        <!--    <div class="content workshop">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--        <form>-->
-        <!--            <input type="email" name="" placeholder="Reserve sua vaga, digite seu email" required/>-->
-        <!--            <input type="hidden" name="nmworkshop" value="">-->
-        <!--            <button>QUERO!</button>-->
-        <!--        </form>-->
-        <!--    </div>-->
+           <div class="content">
+                <p class="hour">15:00</p>
+                <br>
+                <p class="who"> Escola Simonian in Concert</p>
+          </div>
 
+           <div class="content">
+                <p class="hour">17:00</p>
+                <br>
+                <p class="who">Carla Mariani & Banda – Tributo às Divas do Jazz & Blues</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">18:30</p>
+                <br>
+                <p class="who pink">Thiago Espírito Santo & Silvia Goes convidam Alba Santos (Espanha)</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">20:00</p>
+                <br>
+                <p class="who">Igor Willcox 4tet</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">21:30</p>
+                <br>
+                <p class="who">Blubell & Banda</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">23:00</p>
+                <br>
+                <p class="who">Ellén Oléria & Banda</p>
+           </div>
+        </div>
+        
+        <div class="box">
+            <p class="date">30 Julho - Domingo</p>
 
-        <!--</div>-->
-        <!--<div class="box">-->
-        <!--    <p class="date">21 Setembro</p>-->
+            <div class="content">
+                <p class="hour">13:00</p>
+                <br>
+                <p class="who">DJs Santos Jazz + Encontro de Criadores + Foods (full time)</p>
+            </div>
 
-        <!--    <div class="content">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--    </div>-->
+           <div class="content">
+                <p class="hour">14:30</p>
+                <br>
+                <p class="who">Orquestra de Sopros da Banda Marcial de Cubatão – Clássicos do Jazz</p>
+          </div>
 
-        <!--    <div class="content">-->
-                <!--<div class="break">-->
-                <!--    <p class="hour">19:00</p>-->
-                <!--    <p class="who">Coffee Break</p>-->
-                <!--</div>-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-         
-        <!--    </div>-->
-
-        <!--    <div class="content workshop">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--        <form>-->
-        <!--            <input type="email" name="" placeholder="Reserve sua vaga, digite seu email" required/>-->
-        <!--            <input type="hidden" name="nmworkshop" value="">-->
-        <!--            <button>QUERO!</button>-->
-        <!--        </form>-->
-        <!--    </div>-->
-
-        <!--    <div class="content workshop">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--        <form>-->
-        <!--            <input type="email" name="" placeholder="Reserve sua vaga, digite seu email" required/>-->
-        <!--            <input type="hidden" name="nmworkshop" value="">-->
-        <!--            <button>QUERO!</button>-->
-        <!--        </form>-->
-        <!--    </div>-->
-
-        <!--    <div class="content">-->
-                <!--<div class="break">-->
-                <!--    <p class="hour">19:00</p>-->
-                <!--    <p class="who">Coffee Break</p>-->
-                <!--</div>-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-         
-        <!--    </div>-->
-
-        <!--    <div class="content">-->
-        <!--        <p class="hour">19:00</p>-->
-        <!--        <p class="who">Heitor Ramos</p>-->
-        <!--        <br>-->
-        <!--        <p class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>-->
-        <!--    </div>-->
-
-
-        <!--</div>-->
-
+           <div class="content">
+                <p class="hour">16:00</p>
+                <br>
+                <p class="who">Dolores in Blues</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">17:00</p>
+                <br>
+                <p class="who pink">Jes Condado & Banda (Argentina)</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">18:30</p>
+                <br>
+                <p class="who">Roberto Sion & Anaí Rosa</p>
+           </div>
+           
+           <div class="content">
+                <p class="hour">20:00</p>
+                <br>
+                <p class="who">Tradicional Jazz Band</p>
+           </div>
+        </div>
+    
     </section>
     
     <?php include( "../src/newsletter.php") ?>
