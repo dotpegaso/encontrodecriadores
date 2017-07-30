17d74af122fe011124012acd2532c73bad888629
dotpegaso <dotpegaso@gmail.com>
2017-07-30 23:12:27 +0000

    admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -4,16 +4,20 @@ if(window.localStorage.getItem('validation') != 'true'){
     window.location = "https://encontrodecriadores.art/heitor";
 }
 
-$('form').on('submit', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-  
-    var form = $(this);
-    
-    $.ajax({
-     url: '../../src/admin.php',
-     method: 'POST',
-     data: $(form).serialize(),
-     success: function(data){}
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+      
+        var form = $(this);
+        
+        $.ajax({
+         url: '../../src/admin.php',
+         method: 'POST',
+         data: $(form).serialize(),
+         success: function(data){}
+        });
     });
-}); 
+
+});
