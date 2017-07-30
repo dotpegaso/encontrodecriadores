951f0afdca7019367e4b6223bf6279dee4779e5a
André L Benatti <dotpeg@gmail.com>
2017-07-30 22:55:59 +0000

    admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -2,18 +2,16 @@ if(window.localStorage.getItem('validation') != 'true'){
     window.location = "https://encontrodecriadores.art/heitor";
 }
 
-$(document).ready(function(){
-   $('form').on('submit', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-      
-        var form = $(this);
-        
-        $.ajax({
-         url: '../src/admin.php',
-         method: 'POST',
-         data: $(form).serialize(),
-         success: function(data){}
-        });
-    }); 
-});
+$('form').on('submit', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+  
+    var form = $(this);
+    
+    $.ajax({
+     url: '../src/admin.php',
+     method: 'POST',
+     data: $(form).serialize(),
+     success: function(data){}
+    });
+}); 
