1dec631fcd81720b5b088000b6d341236df6ac81
dotpegaso <dotpegaso@gmail.com>
2017-07-31 02:26:59 +0000

    database programacao

--- src/fetch_admin.php
+++ src/fetch_admin.php
@@ -3,7 +3,9 @@
     require_once('../src/dbconn.php');
 
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+    $connection->set_charset("utf8");
     
+    //HOME
     $sql = "SELECT * FROM home";
     $result = $connection->query($sql);
     
@@ -15,6 +17,29 @@
         }
     }
     
+    //TITULO COLAB
+    $sql = "SELECT * FROM colab";
+    $result = $connection->query($sql);
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $colab =  $row['nm_colab'];
+        }
+    }
+    
     $connection->close();
+    
+    
+    //DATA COLAB
+    $sql = "SELECT * FROM colab_data";
+    $result = $connection->query($sql);
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data_um =  $row['data_um'];
+            $data_dois =  $row['data_dois'];
+            $data_tres =  $row['data_tres'];
+        }
+    }
     
 ?>
