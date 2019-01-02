4183d93fccf9e95e13953fd77b20db78c41953e0
dotpegaso <dotpegaso@gmail.com>
2019-01-02 00:22:57 -0200

    novo site do encontro

---
+++ www/precache-manifest.7a7f1f64504992ffa7e7a44bf696646e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "3edfa5208df8341b036f208debe0486c",
+    "url": "/static/media/publico_st.3edfa520.jpg"
+  },
+  {
+    "revision": "07f3673da6595376f11a",
+    "url": "/static/css/main.1cb00842.chunk.css"
+  },
+  {
+    "revision": "a7acfa59b853a1fc9a5b",
+    "url": "/static/js/1.a7acfa59.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e7bf0730936ca6b22362e2b3fa26c25",
+    "url": "/static/media/people.0e7bf073.png"
+  },
+  {
+    "revision": "cc0cfd2c8962e7ff7b4cf312b6c7f307",
+    "url": "/static/media/dario.cc0cfd2c.jpg"
+  },
+  {
+    "revision": "4b561f09b18d783eb9f17a92e941cd8c",
+    "url": "/static/media/bne.4b561f09.jpg"
+  },
+  {
+    "revision": "5fa7ff555949cfa2f2778880ca9170c1",
+    "url": "/static/media/lc.5fa7ff55.jpg"
+  },
+  {
+    "revision": "971b9086bcb0f7044700d5437a8e2326",
+    "url": "/static/media/rafa.971b9086.jpg"
+  },
+  {
+    "revision": "d667ce7b731f3072b541133093e8efc9",
+    "url": "/static/media/xico.d667ce7b.png"
+  },
+  {
+    "revision": "07f3673da6595376f11a",
+    "url": "/static/js/main.07f3673d.chunk.js"
+  },
+  {
+    "revision": "1c9dce854fa925908dda1f75a951443e",
+    "url": "/static/media/publico_nd.1c9dce85.jpg"
+  },
+  {
+    "revision": "f43b36b10fe90a15cfdfcd43fc7ed472",
+    "url": "/static/media/publico_rd.f43b36b1.jpg"
+  },
+  {
+    "revision": "5ddb7ab78b938b0772f6836ab8d6826d",
+    "url": "/static/media/colab_st.5ddb7ab7.png"
+  },
+  {
+    "revision": "bf97fa087bd41e44198b0ed892e396b3",
+    "url": "/static/media/colab_nd.bf97fa08.png"
+  },
+  {
+    "revision": "0e411f1d58063484adfc7941c7c8a924",
+    "url": "/static/media/encontro.0e411f1d.png"
+  },
+  {
+    "revision": "380c56cefb3c2b2e0b42668dda82f233",
+    "url": "/static/media/SpecialElite-Regular.380c56ce.ttf"
+  },
+  {
+    "revision": "1285a8bbbdd63b5d5e911aff1998c04e",
+    "url": "/static/media/NanumGothic-ExtraBold.1285a8bb.ttf"
+  },
+  {
+    "revision": "ffa1619335b8a51c0001cdd53a0efed2",
+    "url": "/static/media/NanumGothic-Regular.ffa16193.ttf"
+  },
+  {
+    "revision": "836e953c04dc7822609e678e924da85e",
+    "url": "/index.html"
+  }
+];
